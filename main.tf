f24ee9d8671ec8dc37d8c166410fb4ae67042f34
mikeCayan99 <mahirturan9097@gmail.com>
2026-04-13 21:06:36 +0200

    feat: TagsFormat

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "azurerm_virtual_network" "vnet" {
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/16"]
-  tags     = local.common_tags
+  tags                = local.common_tags
 }
 
 resource "azurerm_subnet" "subnet" {
