228d2e95f4f0d59bd9ce8dcebdbed13291bc2de1
mikeCayan99 <mahirturan9097@gmail.com>
2026-04-14 20:16:06 +0200

    feat:NSGassoc

--- main.tf
+++ main.tf
@@ -18,3 +18,15 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg-dev-demo"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = local.common_tags
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsg_assoc" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
