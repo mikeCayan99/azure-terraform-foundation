dd8e44a2b6e246782d8398b57aae9a3f6aabca1c
mikeCayan99 <mahirturan9097@gmail.com>
2026-04-20 21:05:41 +0200

    fix: data source auskommentiert

--- data.tf
+++ data.tf
@@ -1,5 +1,5 @@
-data "azurerm_virtual_network" "vnet" {
-  name                = "vnet-dev-demo"
-  resource_group_name = "rg-dev-demo"
-}
+#data "azurerm_virtual_network" "vnet" {
+ # name                = "vnet-dev-demo"
+  #resource_group_name = "rg-dev-demo"
+#}
 
